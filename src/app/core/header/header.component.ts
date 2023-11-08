91c2b7d0debfcb0727081dfa1c786f91d23fb201
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-08 09:03:32 +0100

    Add pb_data and pb_migrations to .gitignore, create HeaderComponent

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <header>
+      <div class="container">
+        <a href="#" routerLink="/" class="logo">ICC AuRA | Proximity</a>
+        <nav>
+          <ul>
+            <li><a href="#" routerLink="/home">Accueil</a></li>
+            <li><a href="#" routerLink="/events">Evènements</a></li>
+            <li><a href="#" routerLink="/appointments">Rendez-vous</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent {
+
+}
