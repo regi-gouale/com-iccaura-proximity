c58304fdb6587051ee0a8d0087e304d746829770
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-08 09:54:52 +0100

    Refactor app component and header styling, add routes to main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./core/header/header.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     template: `
-    <app-header></app-header>
     <router-outlet></router-outlet>
   `,
-    styles: [],
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
-  title = 'com-iccaura-proximity';
+  title = 'ICC AuRA | Proximity';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', loadComponent: () => import('./app/pages/not-found/not-found.component').then(m => m.NotFoundComponent) }
+
+];
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
